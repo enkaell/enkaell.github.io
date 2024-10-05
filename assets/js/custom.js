f5b7189d3851794366658f889606436f56573084
Vladislav Chesnokov <Vladislav.Chesnokov@6ghi.net>
2024-10-05 03:13:22 +0200

    globar refactoring

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,6 @@
 (function($) {
-  var toggle = document.getElementById("menu-toggle");
   var menu = document.getElementById("menu");
   var close = document.getElementById("menu-close");
-
-  toggle.addEventListener("click", function(e) {
-    if (menu.classList.contains("open")) {
-      menu.classList.remove("open");
-    } else {
-      menu.classList.add("open");
-    }
-  });
 
   close.addEventListener("click", function(e) {
     menu.classList.remove("open");
